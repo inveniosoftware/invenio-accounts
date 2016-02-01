0259067ec0338906d165a8f2df795552099dfd0c
Sami Hiltunen <sami.mikael.hiltunen@cern.ch>
2016-02-01 13:47:35 +0100

    tests: pydocstyle>=1.0.0
    * Replaces pep257 with pydocstyle.
    
    Signed-off-by: Sami Hiltunen <sami.mikael.hiltunen@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -23,7 +23,7 @@
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 
-pep257 invenio_accounts && \
+pydocstyle invenio_accounts && \
 check-manifest --ignore ".travis-*" && \
 isort -rc -c -df **/*.py && \
 sphinx-build -qnNW docs docs/_build/html && \
