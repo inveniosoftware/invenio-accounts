1156cc01c020b346fb7ee160ebe367582cf26744
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2017-05-20 10:10:12 +0200

    models: fix typo in field name
    * Renames field operative_system to os. (closes #206)
    
    * Renames field ip_addr to ip.

--- invenio_accounts/alembic/e12419831262_add_new_columns_on_sessionactivity.py
+++ invenio_accounts/alembic/e12419831262_add_new_columns_on_sessionactivity.py
@@ -44,16 +44,16 @@ def upgrade():
         batch_op.add_column(
             sa.Column('device', sa.String(80), nullable=True))
         batch_op.add_column(
-            sa.Column('ip_addr', sa.String(80), nullable=True))
+            sa.Column('ip', sa.String(80), nullable=True))
         batch_op.add_column(
-            sa.Column('operative_system', sa.String(80), nullable=True))
+            sa.Column('os', sa.String(80), nullable=True))
 
 
 def downgrade():
     """Downgrade database."""
     with op.batch_alter_table('accounts_user_session_activity') as batch_op:
-        batch_op.drop_column('operative_system')
-        batch_op.drop_column('ip_addr')
+        batch_op.drop_column('os')
+        batch_op.drop_column('ip')
         batch_op.drop_column('device')
         batch_op.drop_column('country')
         batch_op.drop_column('browser_version')
