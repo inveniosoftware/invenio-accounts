80a581e2b1570e56d515d2e94459237a1b0f986b
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-10-16 15:09:45 +0200

    tests: upgrade to latest pytest-invenio

--- run-tests.sh
+++ run-tests.sh
@@ -6,10 +6,9 @@
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
-
-pydocstyle invenio_accounts tests docs && \
-isort invenio_accounts tests --check-only --diff && \
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test && \
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+python -m check_manifest --ignore ".travis-*" && \
+python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
+python -m pytest
+python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
+tests_exit_code=$?
+exit "$tests_exit_code"
