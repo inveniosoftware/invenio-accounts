02d21b96a2b928ac556762ed970174dcc3a927df
Diego Rodriguez <diego.rodriguez@cern.ch>
2020-12-01 14:41:53 +0100

    global: migrate CI to GH Actions
    * Closes #354.

--- run-tests.sh
+++ run-tests.sh
@@ -6,9 +6,21 @@
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
-python -m check_manifest --ignore ".travis-*" && \
-python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
+
+# Usage:
+#   env DB=postgresql ./run-tests.sh
+
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+docker-services-cli up redis ${DB}
 python -m pytest
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
 tests_exit_code=$?
+docker-services-cli down
 exit "$tests_exit_code"
