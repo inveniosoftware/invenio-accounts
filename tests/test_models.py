65f1d65683461d741a87c73eec064882b88b68fc
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-02-21 18:10:58 +0100

    models: add user profiles as a validated json field
    * dictionary manipulations for the json field will validate the
      operation against the configured schema before actually performing
      them
    * also added two columns for the username on the user model: one storing
      the case-preserving version of the username for display purposes, and
      another storing a lower-case version for ensuring uniqueness of the
      username
    * also added configuration values for customizing the regex according to
      which usernames are validated

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,17 +2,26 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
+# Copyright (C)      2022 TU Wien.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Test invenio-accounts models."""
 
+import pytest
 from invenio_db import db
+from marshmallow import Schema, fields
 from sqlalchemy import inspect
 
 from invenio_accounts import testutils
-from invenio_accounts.models import SessionActivity
+from invenio_accounts.models import SessionActivity, User
+
+
+class CustomProfile(Schema):
+    """A custom user profile schema."""
+
+    file_descriptor = fields.Integer(strict=True)
 
 
 def test_session_activity_model(app):
@@ -65,3 +74,60 @@ def test_session_activity_model(app):
         database.session.commit()
         assert len(user.active_sessions) == 1
         assert user.active_sessions[0].sid_s != session_to_delete.sid_s
+
+
+def test_user_profiles(app):
+    """Test the user profile."""
+    user = User(email="admin@inveniosoftware.org")
+    profile = {
+        "full_name": "Invenio Admin",
+    }
+
+    with pytest.raises(ValueError):
+        # the profile doesn't expect an 'email' value
+        user.user_profile = {
+            **profile, "email": "admin@inveniosoftware.org",
+        }
+
+    assert user.user_profile is None
+
+    # a valid profile should be accepted
+    user.user_profile = profile
+    assert dict(user.user_profile) == profile
+
+    # setting expected properties should work
+    assert len(user.user_profile) == 1
+    assert user.user_profile["full_name"] == "Invenio Admin"
+
+    # but setting unexpected properties should not work
+    with pytest.raises(ValueError):
+        user.user_profile["invalid"] = "value"
+
+    # similar with wrong types for expected fields
+    with pytest.raises(ValueError):
+        user.user_profile["email"] = 1
+
+    assert len(user.user_profile) == 1
+    assert user.user_profile["full_name"] == "Invenio Admin"
+
+
+def test_custom_user_profiles(app):
+    """Test if the customization mechanism for user profiles works."""
+    app.config["ACCOUNTS_USER_PROFILE_SCHEMA"] = CustomProfile()
+    user = User(email="admin@inveniosoftware.org")
+
+    # the default fields aren't allowed in the custom schema
+    with pytest.raises(ValueError):
+        user.user_profile = {
+            "full_name": "Invenio Admin",
+        }
+
+    # the expected properties should work...
+    user.user_profile = {"file_descriptor": 1}
+    assert dict(user.user_profile) == {"file_descriptor": 1}
+
+    # ... but not with unexpected types!
+    with pytest.raises(ValueError):
+        user.user_profile["file_descriptor"] = "1"
+
+    assert dict(user.user_profile) == {"file_descriptor": 1}
