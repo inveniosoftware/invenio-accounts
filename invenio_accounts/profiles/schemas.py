210915f3f32c1d1f6ee8c2f67e1ce77997bc4bfa
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-02-28 14:13:21 +0100

    schemas: add visibility settings to the user preferences

--- invenio_accounts/profiles/schemas.py
+++ invenio_accounts/profiles/schemas.py
@@ -9,7 +9,16 @@
 """Schemas for user profiles and preferences."""
 
 
-from marshmallow import Schema, fields
+from flask_babelex import lazy_gettext as _
+from marshmallow import Schema, ValidationError, fields
+
+
+def validate_visibility(value):
+    """Check if the value is a valid visibility setting."""
+    if value not in ["public", "restricted"]:
+        raise ValidationError(
+            message=str(_("Value must be either 'public' or 'restricted'."))
+        )
 
 
 class UserProfileSchema(Schema):
@@ -20,3 +29,6 @@ class UserProfileSchema(Schema):
 
 class UserPreferencesSchema(Schema):
     """The default schema for user preferences."""
+
+    visibility = fields.String(validate=validate_visibility)
+    email_visibility = fields.String(validate=validate_visibility)
