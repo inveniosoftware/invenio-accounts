9c7b0d8c6badf3a8b2184413a982c55fa9a713ad
Pablo Panero <pablo.panero@cern.ch>
2022-04-20 15:26:34 +0200

    signals: add datastore pre_commit and post_commit

--- invenio_accounts/datastore.py
+++ invenio_accounts/datastore.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2015-2022 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -11,6 +11,7 @@
 from flask_security import SQLAlchemyUserDatastore
 
 from .sessions import delete_user_sessions
+from .signals import datastore_post_commit, datastore_pre_commit
 
 
 class SessionAwareSQLAlchemyUserDatastore(SQLAlchemyUserDatastore):
@@ -27,3 +28,9 @@ class SessionAwareSQLAlchemyUserDatastore(SQLAlchemyUserDatastore):
         if res:
             delete_user_sessions(user)
         return res
+
+    def commit(self):
+        """Commit a user to its session."""
+        datastore_pre_commit.send(session=self.db.session)
+        super().commit()
+        datastore_post_commit.send(session=self.db.session)
