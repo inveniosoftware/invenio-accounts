dba7fa82b37fc366a30993db07599d2bc1301863
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-05-22 00:07:47 +0200

    fix: sphinx deprecation warning for sphinx >= 8
    * the format has changed

--- docs/conf.py
+++ docs/conf.py
@@ -324,8 +324,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "https://docs.python.org/": None,
-    "http://pythonhosted.org/simplekv": None,
+    "python": ("https://docs.python.org/", None),
+    "simplekv": ("http://pythonhosted.org/simplekv", None),
 }
 
 # Autodoc configuraton.
