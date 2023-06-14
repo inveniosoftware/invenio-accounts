b2b171c5f5119ad330d801515449db401da0a980
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-06-14 11:08:43 +0200

    alembic: fix drop constraint command

--- invenio_accounts/alembic/f2522cdd5fcd_change_accountsrole_primary_key_to_string.py
+++ invenio_accounts/alembic/f2522cdd5fcd_change_accountsrole_primary_key_to_string.py
@@ -20,7 +20,7 @@ depends_on = None
 def upgrade():
     """Upgrade database."""
     # Drop primary key and all foreign keys
-    op.execute("ALTER TABLE accounts_role DROP CONSTRAINT pk_accounts_role CASCADE;")
+    op.execute("ALTER TABLE accounts_role DROP CONSTRAINT pk_accounts_role")
 
     op.alter_column(
         "accounts_userrole",
@@ -63,7 +63,7 @@ def downgrade():
     """Downgrade database."""
     # Drop new column `is_managed`
     op.drop_column("accounts_role", "is_managed")
-    op.execute("ALTER TABLE accounts_role DROP CONSTRAINT pk_accounts_role CASCADE;")
+    op.execute("ALTER TABLE accounts_role DROP CONSTRAINT pk_accounts_role;")
 
     op.alter_column(
         "accounts_userrole",
