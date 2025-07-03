a6be3a68c2acaf07b01538e79334587502ab56df
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-07-03 10:59:49 +0200

    release: v6.1.0

--- invenio_accounts/__init__.py
+++ invenio_accounts/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2025 CERN.
-# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024-2025 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -55,7 +55,7 @@ except AttributeError:
 from .ext import InvenioAccounts, InvenioAccountsREST, InvenioAccountsUI
 from .proxies import current_accounts
 
-__version__ = "6.0.1"
+__version__ = "6.1.0"
 
 __all__ = (
     "__version__",
